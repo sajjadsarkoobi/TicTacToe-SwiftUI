0c8af60cc91de344faa4f818948975228b15a9b8
sajjadsarkoobi <sajjadsarkoobi@gmail.com>
2021-07-15 16:08:07 +0300

    Initial Commit

---
+++ TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Sajjad Sarkoobi on 7/15/21.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
